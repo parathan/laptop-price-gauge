2f7ae8d2fa53aa9ed959b7a3777d8e3234499a0b
jaakulan <jaakulans@hotmail.com>
2020-05-01 23:05:48 -0400

    Fixed static and style.css

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def LPG():
+def main():
     return render_template("LPG.html")
+
+if __name__ == "__main__":
+    app.run()
