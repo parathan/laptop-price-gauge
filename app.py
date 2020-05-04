a3d4f91f1e98c03873fd96bbe5684d541d5ae825
jaakulan <jaakulans@hotmail.com>
2020-05-04 15:18:30 -0400

    added a navigation bar, and linked the pages together on it using flask

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://gzfnsdetfsuvth:13c378d6706b5f1ddb71976728e7ff788ed335f2859e883ae65281eb0ec923af@ec2-52-71-231-180.compute-1.amazonaws.com:5432/d4ao2h63kam1sh"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
 
 @app.route("/")
-def main():
-    gpus = Gpus.query.all()
-    cpus = Cpus.query.all()
-    return render_template("LPG.html", gpus=gpus, cpus=cpus)
+def lpg_home():
+    return render_template("LPG_Home.html")
+
+@app.route("/LPG")
+def lpg_tool():
+    return render_template("LPG.html")
+
+@app.route("/LPG_About")
+def lpg_about():
+    return render_template("LPG_About.html")
 
 if __name__ == "__main__":
     app.run()
