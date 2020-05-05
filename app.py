9983062e47933ec82eb4ab1af67745137fa4e4a3
Parathan Vakeesan <parathan243@gmail.com>
2020-05-04 23:46:27 -0400

    Adding searchable dropdown

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
+from models import *
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://gzfnsdetfsuvth:13c378d6706b5f1ddb71976728e7ff788ed335f2859e883ae65281eb0ec923af@ec2-52-71-231-180.compute-1.amazonaws.com:5432/d4ao2h63kam1sh"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
 
 @app.route("/")
 def lpg_home():
@@ -10,7 +13,13 @@ def lpg_home():
 
 @app.route("/LPG")
 def lpg_tool():
-    return render_template("LPG.html")
+    gpus = GPUs.query.all()
+    cpus = CPUs.query.all()
+    return render_template("LPG.html", gpus=gpus, cpus=cpus)
+
+@app.route("/Calculation")
+def calculation():
+    return render_template("LPG_Home.html")
 
 @app.route("/LPG_About")
 def lpg_about():
