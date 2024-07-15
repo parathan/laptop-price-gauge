6dfdce15763ff4b005e45fa81b0cc5438bbdd98f
Parathan <parathan243@gmail.com>
2024-07-15 01:11:56 -0400

    create table sql script

---
+++ database/sql_scripts/cpuCreate.sql
@@ -0,0 +1,10 @@
+	CREATE TABLE component (
+		"Type" VARCHAR(50),
+		"Part Number" VARCHAR(50),
+		"Brand" VARCHAR(50),
+		"Model" VARCHAR(50),
+		"Rank" INTEGER,
+		"Benchmark" FLOAT,
+		"Samples" INTEGER,
+		"URL" VARCHAR(200)
+	)
