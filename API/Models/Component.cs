7a75e344bd2eda684c14c8ab9b8eb3a4db96fb7d
Parathan <parathan243@gmail.com>
2024-07-24 15:04:10 -0400

    set up postgres connection and models

---
+++ API/Models/Component.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("components")]
+    public class Component
+    {
+        [Column("Type")]
+        public string Type { get; set; }
+        [Column("Part Number")]
+        public string PartNumber { get; set; }
+        [Column("Brand")]
+        public string Brand { get; set; }
+        [Column("Model")]
+        public string Model { get; set; }
+        [Column("Rank")]
+        public int Rank { get; set; }
+        [Column("Benchmark")]
+        public float Benchmark { get; set; }
+        [Column("Samples")]
+        public int Samples { get; set; }
+        [Column("URL")]
+        public string URL { get; set; }
+
+    }
+
+}
