5047dc06f1255e91685f768f805c27bd8839d386
Parathan <parathan243@gmail.com>
2024-08-05 18:15:42 -0400

    create basic html for home page

--- lpg-webapp/src/app/page.tsx
+++ lpg-webapp/src/app/page.tsx
@@ -2,6 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Laptop Price Guage</div>
+    <div>
+      <h1>Laptop Price Gauge</h1>
+      <p>
+        Welcome to the Laptop Price Gauge! Here you can see the overall value of
+        the laptop you are looking at based on the components of the laptop, and compare
+        it with the prices that have been provided to you from retailers.
+        <br />
+        The goal of this application is to help you decide which laptop has the most value,
+        and give you more information when looking at a laptop to buy.
+      </p>
+      <button>
+        Go to Laptop Price Gauge
+      </button>
+    </div>
   );
 }
