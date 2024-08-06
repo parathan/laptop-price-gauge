1fb9de6fa1f917b6a71da48be4756f4193b77675
Parathan <parathan243@gmail.com>
2024-08-06 12:43:13 -0400

    add data fetch to form

--- lpg-webapp/src/app/form/page.tsx
+++ lpg-webapp/src/app/form/page.tsx
@@ -1,4 +1,16 @@
-export default function Form() {
+async function getData() {
+    const res = await fetch('http://localhost:5269/api', {cache: 'force-cache'})
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    console.log("got data")
+    return res.json()
+}
+
+export default async function Form() {
+    const data = await getData()
+    console.log(data)
+
     return (
         <div className="grid justify-items-center mx-auto w-1/3 mt-60 space-y-10">
             Form
