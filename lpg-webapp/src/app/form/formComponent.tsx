c0890aec7af340cef06b5f94b4da8c0164fa3d43
Parathan <parathan243@gmail.com>
2024-08-07 15:59:54 -0400

    add form component

---
+++ lpg-webapp/src/app/form/formComponent.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function FormComponent() {
+
+    return (
+        <div>
+            <select>
+                <option>Test Value 1</option>
+                <option>Test Value 2</option>
+            </select>
+        </div>
+    )
+}
--- lpg-webapp/src/app/form/page.tsx
+++ lpg-webapp/src/app/form/page.tsx
@@ -1,9 +1,10 @@
+import FormComponent from "./formComponent"
+
 async function getData() {
     const res = await fetch('http://localhost:5269/api', {cache: 'force-cache'})
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
-    console.log("got data")
     return res.json()
 }
 
@@ -13,7 +14,7 @@ export default async function Form() {
 
     return (
         <div className="grid justify-items-center mx-auto w-1/3 mt-60 space-y-10">
-            Form
+            <FormComponent />
         </div>
     );
 }
