ec4e46f1ac70923c606196d2522ef5786dc60be4
Parathan <parathan243@gmail.com>
2024-08-13 11:37:27 -0400

    init results page

---
+++ lpg-webapp/src/app/results/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Results() {
+    return (
+        <div className="grid justify-items-center mx-auto w-1/3 mt-60 space-y-10">
+            <h1 className="text-3xl font-bold">Results</h1>
+        </div>
+    );
+}
