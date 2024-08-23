58b504e127f2e9a72e0853a2dcd204e8b2c5ef97
Parathan <parathan243@gmail.com>
2024-08-22 23:07:30 -0400

    change results page to client component

--- lpg-webapp/src/app/results/page.tsx
+++ lpg-webapp/src/app/results/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 export default function Results() {
+
+    const [value, setValue] = useState(1000);
+
     return (
         <div className="grid justify-items-center mx-auto w-1/3 mt-60 space-y-10">
             <h1 className="text-3xl font-bold">Results</h1>
+            <p className="text-center">
+                The value of your laptop is ${value}
+            </p>
         </div>
     );
 }
