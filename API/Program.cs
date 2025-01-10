d803d2ab6825d6ed839c21dab94b29b4a1e305ae
Parathan <parathan243@gmail.com>
2025-01-10 14:15:08 -0500

    update unused imports

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,6 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Data;
 using DotNetEnv;
-using Microsoft.AspNetCore.Components.RenderTree;
 
 var builder = WebApplication.CreateBuilder(args);
 
